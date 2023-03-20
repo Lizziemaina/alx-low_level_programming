b31b981e5937437913b765408d6b638323cf677b
Lizziemaina <ewmwanjiru@gmail.com>
2023-03-20 01:00:50 -0700

    task4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+	 * main-Entry point
+	 * Return: Always 0 (Success)
+	 */
+
+int main(void)
+
+{
+	char al, e, q;
+
+	e = 'e';
+	q = 'q';
+	for (al = 'a'; al <= 'z'; al++)
+	{
+		if (al != e && al != q)
+			putchar (al);
+	}
+	putchar('\n');
+
+	return (0);
+}
