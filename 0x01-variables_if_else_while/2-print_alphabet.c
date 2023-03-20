d0b5e029ab29544d44e93e38c4951a13735e963d
Lizziemaina <ewmwanjiru@gmail.com>
2023-03-20 00:01:42 -0700

    task2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,15 +3,15 @@
 /**
  * Main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always (Success)
  */
 
-void main()
+int main(void)
 {
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
 	putchar(c);
-	putchar("\n");
+	putchar('\n');
 	return (0);
 }
