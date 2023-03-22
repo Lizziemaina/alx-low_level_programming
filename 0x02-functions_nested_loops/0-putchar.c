2c4e0a73bdc115bb3797aab33b44128f15b273b7
Lizziemaina <ewmwanjiru@gmail.com>
2023-03-21 23:58:01 -0700

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * main - print _putchar
- * Description - 'prints _putchar to stdout'
- * Return - Always 0 (success)
+ * main - Entry point
+ * Description: prints _putchar to stdout
+ * Return: Always 0 (success)
  */
 int main(void)
 {
@@ -15,5 +15,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	return 0;
+	return (0);
 }
